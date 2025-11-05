23de939697799aef685a5eecde8dfd429658d397
João Áquila <joao.aquila27@gmail.com>
2025-11-05 14:34:34 -0300

    tudo relacionado ao motorista, quase pronto

--- src/usuario.java
+++ src/usuario.java
@@ -1,4 +1,67 @@
-
 public class usuario {
+    String nome;
+    String email;
+    String senha;
+    String cpf;
+    String telefone;
+    avaliacao avaliacao;
 
-}
+    
+    // Construtor
+    public usuario(String nome, String email, String senha, float saldoTotal, String cpf, String telefone) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.cpf = cpf;
+        this.telefone = telefone;
+    }
+
+        // Getters e Setters
+        
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String setSenha() {
+        	return senha;
+        }
+
+        public boolean verificarSenha(String senha) {
+        	return this.senha == senha;
+        }
+
+        public avaliacao getavaliacao() {
+            return avaliacao;
+        }
+
+        public String getCPF() {
+            return cpf;
+        }
+
+        public void setCPF(String cpf) {
+            this.cpf = cpf;
+        }
+
+        public String getTelefone() {
+            return telefone;
+        }
+
+        public void setTelefone(String telefone) {
+            this.telefone = telefone;
+        }
+    }
+
+    
