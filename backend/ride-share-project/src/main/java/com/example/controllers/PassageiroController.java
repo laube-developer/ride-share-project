594b3487fb882afcfb5daee2689834a827956443
Marco Antonio <marcaant.roman@gmail.com>
2025-11-28 20:05:12 -0300

    Atualização no tratamento de mensagens em PassageiroController e utilização de exception UsuarioouSenhaIncorretosException

--- backend/ride-share-project/src/main/java/com/example/controllers/PassageiroController.java
+++ backend/ride-share-project/src/main/java/com/example/controllers/PassageiroController.java
@@ -4,7 +4,7 @@ import com.example.entidades.Passageiro;
 import com.example.enums.StatusCorridaEnum;
 import com.example.exceptions.EstadoInvalidoException;
 import com.example.examples.Operacao;
-import com.example.exceptions.PassageiroInvalidoException;
+import com.example.exceptions.UsuarioouSenhaIncorretosException;
 import com.example.exceptions.SaldoInsuficienteException;
 import com.example.exceptions.PagamentoPendenteException;
 import com.example.entidades.Corrida;
@@ -13,26 +13,22 @@ import com.example.entidades.MeioDePagamento;
 
 import ch.qos.logback.core.status.Status;
 
-
-
-//Nao sei se devo adicionar o UsuarioouSenhaIncorretosException aqui...
-
 public class PassageiroController {
     private static final String NOME_OPERACAO = "ficar-disponivel";
 
-    public static Operacao processarPagamento(MeioDePagamento meioPadrao, Corrida corrida, Passageiro passageiro, int valorParaPagar, int saldo) throws SaldoInsuficienteException, PagamentoPendenteException, PassageiroInvalidoException, EstadoInvalidoException, MetodoPagamentoInexistenteException {
+    public static Operacao processarPagamento(MeioDePagamento meioPadrao, Corrida corrida, Passageiro passageiro, int valorParaPagar, int saldo) throws SaldoInsuficienteException, PagamentoPendenteException, UsuarioouSenhaIncorretosException, EstadoInvalidoException, MetodoPagamentoInexistenteException {
         try {
-            if (passageiro == null) {
-                throw new PassageiroInvalidoException("Passageiro nao informado.");            
+            if (passageiro == null || !passageiro.verificarSenha(passageiro.getSenha())) {
+                throw new UsuarioouSenhaIncorretosException("Usuário ou senha incorretos.");           
             }
             if (saldo < 0){ //saldo negativo, ele ta devendo
-                throw new PagamentoPendenteException();
+                throw new PagamentoPendenteException("Usuário realize o pagamento de suas pendências.");
             }
             if (saldo < valorParaPagar) { //tem mais dinheiro do que de quem deve, mas não tem dinheiro igual
-                throw new SaldoInsuficienteException();
+                throw new SaldoInsuficienteException("Saldo Insuficiente, escolha outra forma de pagamento.");
             }
             if (meioPadrao == null) { // o cara acha q vamos pagar a corrida dele assim na alta po
-                throw new MetodoPagamentoInexistenteException();
+                throw new MetodoPagamentoInexistenteException("Cadastre pelo menos um meio de pagamento.");
             }
             StatusCorridaEnum status = corrida.getStatus();
             if (status == StatusCorridaEnum.EM_ANDAMENTO || status == StatusCorridaEnum.ACEITA) { //o cara ta pensanndo no futuro já, pedindo a proxima corrida, calma calabreso
@@ -41,25 +37,25 @@ public class PassageiroController {
 
             
             //if (meioPadrao(processarPagamento(valorParaPagar)) && corrida.getStatus() == StatusCorridaEnum.EM_ANDAMENTO) {
-            //    throw new PassageiroInvalidoException("Passageiro ja esta em uma corrida.");
+            //    throw new EstadoInvalidoException("Passageiro ja esta em uma corrida.");
             //} //passageiro tentar pagar a corrida antes de finalizar a corrida (menor ideia do que fazer aqui)
 
             boolean temPagamento = passageiro.getMeiosDePagamento() != null
                 && passageiro.getMeiosDePagamento().getTamanho() > 0;
 
             if (!temPagamento) {
-                throw new PassageiroInvalidoException("Cadastre pelo menos um meio de pagamento.");
+                throw new MetodoPagamentoInexistenteException("Cadastre pelo menos um meio de pagamento.");
             }
             // If all checks pass, return a successful Operacao
             return new Operacao(NOME_OPERACAO, true, "Passageiro ficou disponível com sucesso.");
-        } catch (PassageiroInvalidoException e) {
+        } catch (UsuarioouSenhaIncorretosException e) {
             return new Operacao(NOME_OPERACAO, false, e.getMessage());
         } catch (PagamentoPendenteException e) {
-            return new Operacao(NOME_OPERACAO, false, "Erro ao atualizar status do passageiro.");
+            return new Operacao(NOME_OPERACAO, false, e.getMessage());
         } catch (SaldoInsuficienteException e) {
-            return new Operacao(NOME_OPERACAO, false, "Erro ao atualizar status do passageiro.");
+            return new Operacao(NOME_OPERACAO, false, e.getMessage());
         } catch (MetodoPagamentoInexistenteException e) {
-            return new Operacao(NOME_OPERACAO, false, "Erro ao atualizar status do passageiro.");
+            return new Operacao(NOME_OPERACAO, false, e.getMessage());
         } catch (EstadoInvalidoException e) {
             return new Operacao(NOME_OPERACAO, false, "Erro ao atualizar status do passageiro.");
         }
